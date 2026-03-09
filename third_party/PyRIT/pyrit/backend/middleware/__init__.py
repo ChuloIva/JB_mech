2c4a709298cb69044cb0a794f2d43893223c8706
Ivan <chuloiva.work@gmail.com>
2026-03-09 07:41:31 +0100

    removed pyrit

--- third_party/PyRIT/pyrit/backend/middleware/__init__.py
+++
@@ -1,8 +0,0 @@
-# Copyright (c) Microsoft Corporation.
-# Licensed under the MIT license.
-
-"""Middleware module for backend."""
-
-from pyrit.backend.middleware.error_handlers import register_error_handlers
-
-__all__ = ["register_error_handlers"]
--- third_party/PyRIT/pyrit/cli/__init__.py
+++
@@ -1,7 +0,0 @@
-# Copyright (c) Microsoft Corporation.
-# Licensed under the MIT license.
-
-"""
-Module to provide command-line interface functionalities for PyRIT.
-The CLI module is currently experimental.
-"""
